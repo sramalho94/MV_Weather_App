ca7a6fc7fb98fa1110d0642cce6eb997071e3f0d
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-30 12:25:48 -0400

    removed scaffolding and imported SearchBar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <SearchBar />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
+import SearchBar from './components/SearchBar.vue'
 </script>
