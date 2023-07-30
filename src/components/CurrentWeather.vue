629e54a8453026775d350515a4d7a9be276ab6a8
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-30 13:41:57 -0400

    built out CurrentWeather component

---
+++ src/components/CurrentWeather.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card>
+    <v-card-title>{{ store.currentWeather?.locationName }}</v-card-title>
+    <v-card-subtitle>{{ store.currentWeather?.condition }}</v-card-subtitle>
+    <v-card-text>
+      Temperature:
+      {{
+        `${store.currentWeather?.tempFar}°F /  ${store.currentWeather?.tempCel}°C`
+      }}<br />
+      Humidity: {{ store.currentWeather?.humidity }}<br />
+      Wind Speed: {{ store.currentWeather?.windSpeed }}
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useStore } from '@/store'
+
+export default defineComponent({
+  name: 'CurrentWeather',
+  setup() {
+    const store = useStore()
+
+    return {
+      store
+    }
+  }
+})
+</script>
+
+<style scoped></style>
