78e98c94f50da9e193a9101b36dcf8d78c122a43
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-30 13:35:33 -0400

    conditionally rendering CurrentWeather based on store

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,15 @@
   <v-app>
     <v-main>
       <SearchBar />
+      <CurrentWeather v-if="store.currentWeather" />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
+import { useStore } from '@/store'
 import SearchBar from './components/SearchBar.vue'
+import CurrentWeather from './components/CurrentWeather.vue'
+
+const store = useStore()
 </script>
