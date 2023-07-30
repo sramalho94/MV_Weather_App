c9667661aacb7484d054dc5596f5e69d78daafeb
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-30 12:12:21 -0400

    built out SearchBar component that uses pinia store

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-text-field
+    label="Enter Location"
+    v-model="location"
+    @key.enter="searchWeather"
+    outlined
+    clearable
+  >
+    <template v-slot:append><v-btn color="primary">Search</v-btn></template>
+  </v-text-field>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { useStore } from '@/store'
+export default defineComponent({
+  name: 'SearchBar',
+
+  setup() {
+    const store = useStore()
+    const location = ref('')
+
+    const searchWeather = async () => {
+      if (!location.value) return
+      store.setLocation(location.value)
+      await store.fetchCurrentWeather()
+      await store.fetchForecast()
+      location.value = ''
+    }
+
+    return {
+      location,
+      searchWeather
+    }
+  }
+})
+</script>
+
+<style scoped></style>
