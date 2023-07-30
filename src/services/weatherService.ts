5d72422a492df10c47dea1f50b920d978e7ec233
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-30 15:34:48 -0400

    refactored fetchForecast to align with data returned from API

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 // function to get currentWeather
 export async function fetchCurrentWeather(location: string) {
-  console.log('fetchCurrentWeather called with location: ', location)
+  // console.log('fetchCurrentWeather called with location: ', location)
   try {
     // Get current weather data and coordinates
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`
@@ -28,7 +28,7 @@ export async function fetchCurrentWeather(location: string) {
     weatherData.main.tempCel = Math.round(kelvin - 273.15)
     weatherData.main.tempFar = Math.round(((kelvin - 273.15) * 9) / 5 + 32)
 
-    console.log('fetchCurrentWeather response: ', weatherData)
+    // console.log('fetchCurrentWeather response: ', weatherData)
     return weatherData
   } catch (error) {
     console.error(
@@ -54,11 +54,26 @@ export async function fetchForecast(location: string) {
       `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${API_KEY}`
     )
 
-    // Extract 5 days forecast data
-    const fiveDayForecast = forecastResponse.data.daily.slice(0, 5)
+    console.log('forecastResponse.data: ', forecastResponse.data)
+    // Ensure that data is available before trying to access it
+    if (forecastResponse.data && forecastResponse.data.daily) {
+      // Extract 5 days forecast data
+      const fiveDayForecast = forecastResponse.data.daily.slice(0, 5)
 
-    console.log('fetchForecast response: ', fiveDayForecast)
-    return fiveDayForecast
+      // Process the data into the desired format
+      const processedForecast = fiveDayForecast.map((day: any) => {
+        return {
+          date: day.dt,
+          temperature: day.temp.day,
+          weather: day.weather[0].description
+        }
+      })
+
+      console.log('fetchForecast response: ', processedForecast)
+      return processedForecast
+    } else {
+      throw new Error('No forecast data available.')
+    }
   } catch (error) {
     console.error(`Error fetching forecast for location "${location}":`, error)
     throw error
