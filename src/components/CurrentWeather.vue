4e16a0f10e4cb70aebd3a193c234119ace73e424
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-30 15:56:10 -0400

    style changes to decrease the width of the component

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -1,16 +1,24 @@
 <template>
-  <v-card>
-    <v-card-title>{{ store.currentWeather?.locationName }}</v-card-title>
-    <v-card-subtitle>{{ store.currentWeather?.condition }}</v-card-subtitle>
-    <v-card-text>
-      Temperature:
-      {{
-        `${store.currentWeather?.tempFar}°F /  ${store.currentWeather?.tempCel}°C`
-      }}<br />
-      Humidity: {{ store.currentWeather?.humidity }}<br />
-      Wind Speed: {{ store.currentWeather?.windSpeed }}
-    </v-card-text>
-  </v-card>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="8" md="4">
+        <v-card>
+          <v-card-title>{{ store.currentWeather?.locationName }}</v-card-title>
+          <v-card-subtitle>{{
+            store.currentWeather?.condition
+          }}</v-card-subtitle>
+          <v-card-text>
+            Temperature:
+            {{
+              `${store.currentWeather?.tempFar}°F /  ${store.currentWeather?.tempCel}°C`
+            }}<br />
+            Humidity: {{ store.currentWeather?.humidity }}<br />
+            Wind Speed: {{ store.currentWeather?.windSpeed }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
