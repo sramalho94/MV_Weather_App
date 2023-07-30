86de4017e44a547bb45f4388bd0f998520480013
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-30 12:24:23 -0400

    registerPlugins now takes pinia as an arguement

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -7,14 +7,12 @@
 // Plugins
 import { loadFonts } from './webfontloader'
 import vuetify from './vuetify'
-import pinia from '../store'
 
 // Types
 import type { App } from 'vue'
+import type { Pinia } from 'pinia'
 
-export function registerPlugins (app: App) {
+export function registerPlugins(app: App, pinia: Pinia) {
   loadFonts()
-  app
-    .use(vuetify)
-    .use(pinia)
+  app.use(vuetify).use(pinia)
 }
